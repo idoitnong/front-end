0a9c11012d542fba375d4b45e869d99b82901fba
MYEONGGWANG BANG <idsan.net@hotmail.com>
2023-12-02 17:02:00 +0900

    Remove dotenv package, use 'VITE_' prefix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import "dotenv/config";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          (process.env.NODE_ENV === "production" ? process.env.API_URL : process.env.API_URL_DEV) ||
-          "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:3000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
